0eb1227b1df47501e0b2d677ed44e370b83a32f5
ksquires <ksquires11@gmail.com>
2018-05-04 19:59:30 -0600

    FirstWorkDone

---
+++ GameJam2018Force/Assets/GameManagerScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour {
+    public GameObject player = null;
+    public Debris selected = null;
+    public static GameManagerScript gameManager;
+	// Use this for initialization
+	void Start () {
+        gameManager = this;
+        player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
